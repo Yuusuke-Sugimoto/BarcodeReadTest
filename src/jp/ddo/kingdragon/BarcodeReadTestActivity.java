7708d907227e706250a4ac2df51ad9d69b1e2fad
Yuusuke Sugimoto <yuusuke.sugimoto@gmail.com>
2012-05-23 03:20:06 +0900

    テストコミット

---
+++ src/jp/ddo/kingdragon/BarcodeReadTestActivity.java
@@ -0,0 +1,12 @@
+package jp.ddo.kingdragon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BarcodeReadTestActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
